46536713fc66b6ae65ad128111a0a11d4193f446
jimstev2447 <jim.stevenson2447@gmail.com>
2023-08-14 21:23:58 +0100

    add token structures

---
+++ tokenizer.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const data = fs.readFileSync("./test.txt", "utf-8");
+const { stdout, stderr } = require("process");
+
+function reportError(line, where, message) {
+  stderr.write(`[Line: ${line}]: Error ${where} : ${message}`);
+}
+
+function createToken(type, lexeme, literal, line) {
+  return {
+    type,
+    lexeme,
+    literal,
+    line,
+  };
+}
+
+const tokenTypes = {
+  "🌕": 'true',
+  "🌑": 'false',
+  "📜": "string",
+  "🧠": " variable",
+  "👍": "plus",
+  "👎": "minus",
+  "🖖": "divide",
+  "🤞": "multiply",
+  "🤙": "modulo",
+  "👥": "equal",
+  "🗻": "greater",
+  "🏖️": "less",
+  "🐱": "openBrace",
+  "🐶": "closeBrace",
+  "🍺": "openParen",
+  "🍔": "closeParen",
+  "❌": "not",
+  "🤷": "or",
+  "🙌": "and",
+  "👀": "if",
+  "⭐️": "ifTrue",
+  "🔥": "ifFalse",
+  "🏭": "function",
+  "💨": "functionInvokation",
+  "🎁": "return",
+  "🪦": "endOfStatement",
+  "🗣️": "print",
+};
